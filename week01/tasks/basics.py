c6e611fed78863af10dc9c1944fb91fed7cb475a
szalerg <szalegr_aehit@students.vizja.pl>
2025-09-04 19:24:04 +0200

    Implement zadanie_1 and update test

---
+++ week01/tasks/basics.py
@@ -0,0 +1,18 @@
+# 1_basics.py
+"""
+Proste zadania z Pythona: zmienne, warunki, pętle, funkcje.
+Każde zadanie ma poziom trudności: ⭐, ⭐⭐, ⭐⭐⭐
+"""
+
+def zadanie_1(a, b):
+    # ⭐ Napisz funkcję, która zwraca sumę dwóch liczb
+
+    return a + b
+
+def zadanie_2():
+    # ⭐⭐ Napisz funkcję, która sprawdza, czy liczba jest parzysta
+    pass
+
+def zadanie_3():
+    # ⭐⭐⭐ Napisz funkcję, która zwraca największy wspólny dzielnik dwóch liczb
+    pass
---
+++ week01/tests/test_basics.py
@@ -0,0 +1,9 @@
+# test_basics.py
+"""
+Testy automatyczne dla zadań podstawowych
+"""
+import pytest
+from week01.tasks import basics
+
+def test_zadanie_1():
+    assert basics.zadanie_1(2, 3) == 5  # Tu później podstawić implementację
