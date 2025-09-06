654182c04d28ca18ae48af2cce2e1a66803f817e
szalerg <szalegr_aehit@students.vizja.pl>
2025-09-06 19:48:39 +0200

    Dodano kalkulator tygodnia z obsługą +, -, *, /, mod, ^, sqrt

--- week01/code_review/explain_code.md
+++ week01/code_review/explain_code.md
@@ -2,3 +2,15 @@
 """
 Zadanie: przeanalizuj powyższy kod i opisz, co jest źle, jak naprawić, co można ulepszyć
 """
+Błąd: Funkcji dodaj wykonuje odejmowanie zamiast dodawania.
+Poprawka: Zmienić 'return a - b' na 'return a + b'.
+Ulepszenie: Dodać docstring i typy argumentów:
+def dodaj(a: int, b: int) -> int:
+    """Zwraca sumę dwóch liczb całkowitych."""
+
+Błąd: Brak obsługi dzielenia przez zero.
+Poprawka: Dodać warunek:
+if b == 0:
+    raise ValueError("Nie można dzielić przez zero")
+return a / b
+Ulepszenie: Dodać docstring i typy argumentów.
--- week01/projects/calculator.py
+++ week01/projects/calculator.py
@@ -3,6 +3,35 @@
 Projekt tygodnia: prosty kalkulator tekstowy.
 Obsługuje: +, -, *, /, mod, ^, sqrt, nawiasy, błędy dzielenia przez 0
 """
+import math
 
 def kalkulator():
-    pass  # Implementacja kalkulatora
+    """
+    Prosty kalkulator tekstowy.
+    Kalkulator obsługej działania: +, -, *, /, mod, ^, sqrt
+    Cały program działa w pętli, aż użytkownik wpuisze 'exit'.
+    """
+    while True:
+        expression = input("Podaje działanie (np. 2 + 3, 4 * 5, sqrt(9)): ")    # Pobierania wyrażeń od użytkownika
+
+
+        if expression.lower() == "exit":                                        # Warunek wyjścia z pętli
+            print("Wyłączony")
+            break
+
+        expression = expression.replace("mod", "%")                             # Dzielenie
+        expression = expression.replace("^", "**")                              # Potęgowanie
+        expression = expression.replace("sqrt", "math.sqrt")                    # Pierwiastek kwadratowy
+
+        try:                                                                            
+                                                                                # Liczenie wyniku wyrażeń
+            result = eval(expression)
+
+            print("Wynik:", round(result, 2))
+
+        except ZeroDivisionError:
+            print("Błąd dzielenia przez zero!")                                 # Obsługa dzielenia przez zero
+
+        except Exception as e: 
+
+            print("Błąd w wyrażeniu:", e)                                       # Obsługa innych błędów 
