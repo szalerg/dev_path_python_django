ad25eea1e5d38f9049364dbcba7b23f65337cfbb
szalerg <szalegr_aehit@students.vizja.pl>
2025-09-17 18:56:42 +0200

    Dodanie funkcji wypisywania liczby od 1 do n, sume liczby od 1 do n, zwracania silni oraz testy

--- week03/tasks/loops.py
+++ week03/tasks/loops.py
@@ -2,14 +2,21 @@
 
 def zadanie_1(n):
     # ⭐ Napisz funkcję, która wypisuje liczby od 1 do n
-    pass
+    for i in range(1, n + 1):
+        print(i)
 
 
 def zadanie_2(n):
     # ⭐⭐ Napisz funkcję, która zwraca sumę liczb od 1 do n
-    pass
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+    return total
 
 
 def zadanie_3(n):
     # ⭐⭐⭐ Napisz funkcję, która zwraca n! (silnię)
-    pass
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
