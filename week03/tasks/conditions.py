0f0a2b7336b2059e6e4847d21e93acdf9109232a
szalerg <szalegr_aehit@students.vizja.pl>
2025-09-24 18:33:15 +0200

    Dodanie funkcji sprawdzania liczby dodatniej, ujemnej, zero, funkcji sprawdzania rok przystępnego, funkcji sprawdzania czy z boków można zbudować trójkąt

--- week03/tasks/conditions.py
+++ week03/tasks/conditions.py
@@ -2,14 +2,34 @@
 
 def zadanie_1(x):
     # ⭐ Napisz funkcję, która sprawdza, czy liczba jest dodatnia, ujemna czy zero
-    pass
+    if x > 0:
+        return("dodatnia")
+    elif x == 0:
+        return("zero")
+    else:
+        return("ujemna")
 
 
 def zadanie_2(year):
     # ⭐⭐ Napisz funkcję, która sprawdza, czy rok jest przestępny
-    pass
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
 
 
 def zadanie_3(a, b, c):
     # ⭐⭐⭐ Napisz funkcję, która sprawdza, czy z boków a, b, c można zbudować trójkąt
-    pass
+    if a + b > c and a + c > b and b + c > a:
+        return True
+    else:
+        return False
+        
+    
+        
+    
+       
