8450d7df5eb7b18f83963f5e902d885a8a7189df
szalerg <szalegr_aehit@students.vizja.pl>
2025-09-25 18:44:25 +0200

    Utworzenie gry zgadnij liczbę

--- week03/projects/guess_the_number.py
+++ week03/projects/guess_the_number.py
@@ -9,3 +9,30 @@ Projekt tygodnia: Gra "Zgadnij liczbę"
    - "Zgadłeś!"
 3. Gra trwa do odgadnięcia liczby.
 """
+import random
+
+def mini_game():
+   while True:  # pętla główna gry
+      RNG = random.randint(1, 100)
+      number_of_trials = 0
+      
+      while True:  # pętla zgadywania
+         print("Zgadnij liczbę od 1 do 100")
+         guess = int(input("Wpisz liczbę: "))
+         number_of_trials += 1
+         
+         if guess < RNG:
+            print("Za mało!")
+         elif guess > RNG:
+            print("Za dużo!")
+         else:
+            print("Zgadłeś!")
+            print(f"Udało Ci się w {number_of_trials} próbach.")
+            break
+      
+      play_again = input("Czy chcesz zagrać ponownie? (tak/nie): ")
+      if play_again.lower() != "tak":
+         print("Dzięki za grę! 👋")
+         break
+
+mini_game()         
