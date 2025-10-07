03e2d86f0a82635861ee07692f82da19aa7bce02
szalerg <szalegr_aehit@students.vizja.pl>
2025-10-07 18:09:21 +0200

    Dodanie class Person i metode greet()

--- week04/tasks/classes.py
+++ week04/tasks/classes.py
@@ -1,5 +1,15 @@
 #Prosta klasa
 #Stwórz klasę Person, która ma atrybuty name i age, oraz metodę greet(), która zwraca "Hello, my name is <name>".
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f"Person: {self.name}"
+    def greet(self):
+        return f"Hello, my name is {self.name}"
 
 #Klasa BankAccount
 #Stwórz klasę BankAccount z metodami deposit(amount), withdraw(amount) i get_balance(). Saldo początkowe to 0, nie można wypłacić więcej niż jest na koncie
+
