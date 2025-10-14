7bed251c672bbf443d58ea2446ad15e04c3c1337
szalerg <szalegr_aehit@students.vizja.pl>
2025-10-14 17:10:54 +0200

    stworzenie programu Mini Notatnik

--- week04/projects/main.py
+++ week04/projects/main.py
@@ -8,3 +8,23 @@ wyświetli wszystkie notatki (odczyt pliku),
 
 umożliwia zakończenie programu wpisując "exit".
 """
+
+def mini_notebook():
+
+    while True:
+        user_input = input("Wpisz notatkę ('show' aby wyświetlić, 'exit' aby zakończyć): ")
+
+        if user_input == "exit":
+            break
+
+        elif user_input == "show":
+            try:
+                with open("notes.txt", "r") as file:
+                    for line in file:
+                        print(line.strip())
+            except FileNotFoundError:
+                print("Brak notatek.")            
+            
+        else:
+            with open("notes.txt", "a") as file:
+                file.write(user_input + "\n")
